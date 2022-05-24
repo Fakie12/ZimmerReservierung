a6b208049c20172fcfcd05ae4759f34f92a3e889
Fakie12 <fakiedongo@gmail.com>
2022-05-24 22:51:48 +0200

    initial commit

---
+++ src/main/java/com/projekt1/zimmerreservierung/ZimmerReservierungApplication.java
@@ -0,0 +1,13 @@
+package com.projekt1.zimmerreservierung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZimmerReservierungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZimmerReservierungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projekt1/zimmerreservierung/ZimmerReservierungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekt1.zimmerreservierung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZimmerReservierungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
